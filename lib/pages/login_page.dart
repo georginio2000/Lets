e26b50d754255def69f19f6844bb33f5e8b1b1c7
nikolaos <nikolaosbeligiannis@gmail.com>
2024-12-26 18:31:39 +0200

    _1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,7 +3,7 @@ import '../widgets/login_button.dart';
 import '../widgets/forgot_your_password.dart';
 import '../pages/registration_page.dart';
 import '../widgets/sign_in_with.dart';
-
+import '../widgets/dont_have_account.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -189,49 +189,15 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(height: 6),
 
                 // DONT HAVE AN ACCOUNT? SIGN UP TEXT-BUTTON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "DON'T HAVE AN ACCOUNT?",
-                      style: TextStyle(
-                        fontSize: 5,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                DontHaveAccount(
+                  onButtonPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegistrationPage(),
                       ),
-                    ),
-                    const SizedBox(width: 7),
-                    Container(
-                      width: 70,
-                      height: 23,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(0),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1.0,
-                        ),
-                        color: const Color(0xFFE5E1DA),
-                      ),
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const RegistrationPage(),
-                            ),
-                          );
-                        },
-                        child: const Text(
-                          "SIGN UP",
-                          style: TextStyle(
-                            fontSize: 5,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
 
                 const SizedBox(height: 30),
---
+++ lib/widgets/dont_have_account.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class DontHaveAccount extends StatelessWidget {
+  final VoidCallback onButtonPressed;
+
+  const DontHaveAccount({
+    Key? key,
+    required this.onButtonPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "DON'T HAVE AN ACCOUNT?",
+          style: TextStyle(
+            fontSize: 5,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(width: 7),
+        Container(
+          width: 70,
+          height: 23,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0,
+            ),
+            color: const Color(0xFFE5E1DA),
+          ),
+          child: TextButton(
+            onPressed: onButtonPressed,
+            child: const Text(
+              "SIGN UP",
+              style: TextStyle(
+                fontSize: 5,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
