ba39a0c8da5a661d8de34e27267b0ff7f0590f40
nikolaos <nikolaosbeligiannis@gmail.com>
2024-12-26 14:11:39 +0200

    _1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Αρχικοποίηση Firebase
   runApp(const MyApp());
 }
 
