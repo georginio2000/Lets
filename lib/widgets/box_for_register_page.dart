b6c151172c8b35ff1cd0b6c7af87bd819772450b
nikolaos <nikolaosbeligiannis@gmail.com>
2024-12-26 19:40:02 +0200

    _1

--- lib/widgets/box_for_register_page.dart
+++ lib/widgets/box_for_register_page.dart
@@ -7,12 +7,12 @@ class BoxForRegisterPage extends StatelessWidget {
   final TextInputType keyboardType;
 
   const BoxForRegisterPage({
-    Key? key,
+    super.key,
     required this.hintText,
     required this.controller,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
