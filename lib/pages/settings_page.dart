5b401528a6a0ab0193e4693b77c62a9b195a0992
nikolaos <nikolaosbeligiannis@gmail.com>
2024-12-27 13:11:44 +0200

    _1

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -6,80 +6,95 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: const Color(0xFF9CC4C4), // background xrwma
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SettingsBox(
-                  labelText: "VIEW YOUR INFO",
-                  onPressed: (){
-                    print("VIEW YOUR INFO");
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "CHANGE YOUR PASSWORD",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "CHANGE YOUR EMAIL",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "EMAIL NOTIFICATIONS",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "PUSH NOTIFICATIONS",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "LANGUAGE",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "FAQ",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "FEEDBACK AND SUPPORT",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "ABOUT LETS",
-                  onPressed: (){
-                  },
-                ),
-                const SizedBox(height: 30),
-                SettingsBox(
-                  labelText: "LOGOUT",
-                  onPressed: (){
-                  },
-                ),
-              ]
+      backgroundColor: const Color(0xFF9CC4C4), // Background color
+      body: Stack(
+        children: [
+          // Back button
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF50B498), // Button background color
+                border: Border.all(color: Colors.black, width: 1), // Border
+                borderRadius: BorderRadius.circular(2),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                color: Colors.black, // Icon color
+                iconSize: 24, // Icon size
+                onPressed: () {
+                  Navigator.pop(context); // Back to the previous page
+                },
+              ),
             ),
           ),
-        ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 80),
+                    SettingsBox(
+                      labelText: "VIEW YOUR INFO",
+                      onPressed: () {
+                        print("VIEW YOUR INFO");
+                      },
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "CHANGE YOUR PASSWORD",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "CHANGE YOUR EMAIL",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "EMAIL NOTIFICATIONS",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "PUSH NOTIFICATIONS",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "LANGUAGE",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "FAQ",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "FEEDBACK AND SUPPORT",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "ABOUT LETS",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    SettingsBox(
+                      labelText: "LOGOUT",
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/search_activity_bot.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class SearchWithFilter extends StatelessWidget {
+  final TextEditingController searchController;
+  final VoidCallback onSearchPressed;
+  final VoidCallback onFilterPressed;
+
+  const SearchWithFilter({
+    Key? key,
+    required this.searchController,
+    required this.onSearchPressed,
+    required this.onFilterPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // Search Input Box
+        Expanded(
+          child: Container(
+            height: 45,
+            decoration: BoxDecoration(
+              color: const Color(0xFFE5E1DA),
+              border: Border.all(
+                color: Colors.black,
+                width: 1.0,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 1),
+                ),
+              ],
+            ),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'SEARCH ACTIVITY ID',
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black38,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+              ),
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10), // Space between input and filter button
+
+        // Filter Button
+        Container(
+          width: 45,
+          height: 45,
+          decoration: BoxDecoration(
+            color: const Color(0xFF50B498),
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(color: Colors.black, width: 1.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.filter_alt),
+            color: Colors.black,
+            iconSize: 24,
+            onPressed: onFilterPressed,
+          ),
+        ),
+      ],
+    );
+  }
+}
