8ddda5eebae42ce98fd9209ee8d664a0e54a4060
nikolaos <nikolaosbeligiannis@gmail.com>
2024-12-29 15:21:30 +0200

    refresh sto profile_page ?? top

--- lib/widgets/my_profile_page/search_user_box.dart
+++ lib/widgets/my_profile_page/search_user_box.dart
@@ -15,50 +15,63 @@ class CustomSearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 260,
-      height: 30, // Set fixed height
+      height: 30, // Fixed height
       decoration: BoxDecoration(
-        color: const Color(0xFFE5E1DA), // Ανοιχτό μπεζ φόντο
+        color: const Color(0xFFE5E1DA),
         borderRadius: BorderRadius.circular(0),
         border: Border.all(color: Colors.black, width: 1.0),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(1),
             blurRadius: 5,
-            offset: const Offset(2, 4), // Σκιά
+            offset: const Offset(2, 4),
           ),
         ],
       ),
-      child: Row(
+      child: Stack(
         children: [
-          Expanded(
-            child: TextField(
-              controller: controller,
-              style: const TextStyle(
-                fontSize: 14,
-                color: Colors.black, // Χρώμα του κειμένου που πληκτρολογείται
-              ),
-              decoration: const InputDecoration(
-                hintText: "SEARCH USERNAME",
-                hintStyle: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey, // Αχνό γκρι χρώμα για το hint text
+          // TextField
+          Positioned(
+            left: 10,
+            top: -8,
+            right: 40,
+            child: SizedBox(
+              height: 30,
+              child: TextField(
+                controller: controller,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
                 ),
-                border: InputBorder.none, // Αφαίρεση του default περιγράμματος
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 11, // Center vertically
-                  horizontal: 10, // Padding on the left and right
+                decoration: const InputDecoration(
+                  hintText: "SEARCH USERNAME",
+                  hintStyle: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 8, // Center vertically
+                  ),
                 ),
+                textAlignVertical: TextAlignVertical.center,
               ),
-              textAlignVertical: TextAlignVertical.center, // Ensures text stays centered
             ),
           ),
-          IconButton(
-            icon: SvgPicture.asset(
-              'assets/SEARCH_ICON.svg', // Εικονίδιο αναζήτησης
-              width: 50,
-              height: 20,
+          Positioned(
+            right: 4,
+            top: 4,
+            child: GestureDetector(
+              onTap: onSearchPressed,
+              child: SizedBox(
+                width: 25,
+                height: 25,
+                child: SvgPicture.asset(
+                  'assets/SEARCH_ICON.svg',
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
-            onPressed: onSearchPressed,
           ),
         ],
       ),
