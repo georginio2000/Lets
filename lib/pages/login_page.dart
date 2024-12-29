dc6bc32b46dcbe05fb97af26400a4aed46d03d1a
Georgios Kouseris <g.kouseris22@gmail.com>
2024-12-29 20:29:41 +0200

    just merged all the progresss

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => const MainScreen(),
+          builder: (context) => MainScreen(),
         ),
       );
     } catch (e) {
