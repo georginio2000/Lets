fefb3b37b73161b10f5614fd417d132a37d7efd8
nikolaj <101004317+nikolaosss@users.noreply.github.com>
2025-01-12 20:31:12 +0200

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -75,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                   Navigator.pop(context, 'same');
                 },
                 child: const Text(
-                  'same',
+                  'Same',
                   style: TextStyle(color: Colors.black), // Text color
                 ),
               ),
